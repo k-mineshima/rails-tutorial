8bf627f6aac3ef46a847c8ab719d8471a27a434f
峯島和希 <mineshimakazuki@mineshimakazukinoMacBook-Pro.local>
2022-02-08 13:55:55 +0900

    [add] jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
+import jquery from 'jquery';
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+window.$ = jquery;
+window.jQuery = jquery;
